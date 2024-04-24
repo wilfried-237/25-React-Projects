ab32dfbad4f780ebfe0e1c99f83af4b68d1f4cc4
Ulrich Wilfried <ulrichwilfried1234@gmail.com>
2024-04-24 17:56:49 +0100

    Search Auto Complete Done

--- src/components/Search-Auto-Complete-API/SearchAutoComplete.jsx
+++ src/components/Search-Auto-Complete-API/SearchAutoComplete.jsx
@@ -1,72 +1,78 @@
-import { useState } from 'react'
-import './index.css'
+import { useEffect, useState } from "react";
+// import data from './data';
+import "./index.css";
 
 export default function SearchAutoComplete() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [getData, setGetData] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
 
-    const [searchTerm, setSearchTerm] = useState("ma");
-    const [getData, setGetData] = useState([]);
-    const [searchResults, setSearchResults] = useState([]);
-    const [loading, setLoading] = useState(false);
-  const [getErrors, setGetErrors] = useState(null);
-    
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function fetchData(){
-        try{
-            setLoading(true);
-            const response = await fetch(`https://dummyjson.com/products?limit=1000`);
-            const data = await response.json();
+  async function fetchData() {
+    try {
+      setIsLoading(true);
 
-            if(data){
-                setGetData(data.products);
-                
-                console.log(getData);
-                
-            }
-        }catch(e){
-            setGetErrors(e.message);
-            setLoading(false);
-        }      
+      const response = await fetch(`https://dummyjson.com/users?limit=1000`);
+      const data = await response.json();
+      
+
+      if (data) {
+        setGetData(data.users.map((user)=> user.firstName.toLowerCase()));
+        setIsLoading(false);
+      }
+    } catch (e) {
+      setIsLoading(false);
+      setError(e.message);
     }
+  }
 
-    useState(()=>{
-        fetchData();
+  function handleChange(e){
+    setSearchTerm(e.target.value.toLowerCase())
+    const query = e.target.value.toLowerCase();
 
-        if(searchTerm && searchTerm.length > 1){
-            let copyData = [...getData];
+    if(query.length > 1){
+        setSearchResults(getData.filter((name)=> name.indexOf(query) > -1))
+    }else{
+        setSearchResults([])
+    }
+  }
 
-            console.log(copyData);
+  function handleClick(getCurrentIndex){
+    setSearchTerm(searchResults[getCurrentIndex])
+    setSearchResults([])
+  }
 
-            
+  console.log(getData)
+  console.log(searchResults)
 
-            setSearchResults(copyData);
-            setLoading(false);
-        }
-    }, [searchTerm])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    if (getErrors) {
-        return <h1>{getErrors}</h1>;
-      }
-    
-      if (loading) {
-        return <h1>Data Loading ! Please Wait...</h1>;
-      }
 
+  if (isLoading) {
+    return <div>Loading data please Wait...</div>;
+  }
+
+  if (error !== null) {
+    return <div>An Error Occured ! {error}</div>;
+  }
 
   return (
-    <div className='SearchAutoComplete'>
-        <input type="text" onChange={(e)=> setSearchTerm(e.target.value)} placeholder='Enter Search Term...' />
-        <div>
-            <ul>
-                {
-                    searchResults && searchResults.length > 0? 
-                    searchResults.map((product, index)=>{
-                                <li key={index}>{product.title}</li>
-                            }
-                        )
-                    :null
-                }
-            </ul>
-        </div>
+    <div className="SearchAutoComplete">
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => handleChange(e)}
+        placeholder="Enter Search Term..."
+      />
+      <div>
+        <ul className="auto-complete">
+          {searchResults.map((name, index) => <li key={index} onClick={()=> handleClick(index)}>{name}</li>)}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
